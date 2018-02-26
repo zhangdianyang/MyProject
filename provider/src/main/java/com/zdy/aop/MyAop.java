b3aa55e66fee71021a8dc82eafd8dd71b00e44a1
zhangdianyang_dz <zhangdianyang_dz@mayi.com>
2018-02-26 10:00:30 +0800

    数据库配置

--- provider/src/main/java/com/zdy/aop/MyAop.java
+++ provider/src/main/java/com/zdy/aop/MyAop.java
@@ -4,6 +4,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,17 @@ import org.springframework.stereotype.Component;
 @Order(1)
 public class MyAop {
 
+    private Logger logger = LoggerFactory.getLogger(MyAop.class);
+
     @Pointcut(value = "execution(* com.zdy.service..*.*(..))")
     public void pointCutMethod(){}
 
     @Around("pointCutMethod()")
     public Object aroundService(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
         Object result = proceedingJoinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        logger.info("耗时="+(endTime-startTime)+"毫秒");
         return result;
     }
 
