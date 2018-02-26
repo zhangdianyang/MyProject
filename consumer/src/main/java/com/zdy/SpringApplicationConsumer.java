a8635775a2ab262d124dd642de604b402764a9b5
zhangdianyang_dz <zhangdianyang_dz@mayi.com>
2018-02-26 17:49:09 +0800

    数据库配置

--- consumer/src/main/java/com/zdy/SpringApplicationConsumer.java
+++ consumer/src/main/java/com/zdy/SpringApplicationConsumer.java
@@ -1,13 +1,18 @@
 package com.zdy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author zdy
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @ImportResource(value = {"classpath:consumer.xml"})
 public class SpringApplicationConsumer {
 
