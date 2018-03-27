f29a128e54806dfb0083d890217e82fbad591d8f
zhangdianyang_dz <zhangdianyang_dz@mayi.com>
2018-03-27 15:43:36 +0800

    io测试

---
+++ consumer/src/main/java/com/zdy/intercetors/MyBodyAdvice.java
@@ -0,0 +1,26 @@
+package com.zdy.intercetors;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+
+/**
+ * @author zdy
+ */
+@ControllerAdvice
+public class MyBodyAdvice implements ResponseBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return null;
+    }
+}
