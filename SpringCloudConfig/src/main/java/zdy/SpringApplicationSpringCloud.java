f282be2beb5196ae2851e25c4226db5f1192e828
zhangdianyang_dz <zhangdianyang_dz@mayi.com>
2018-06-28 12:47:52 +0800

    spring cloud搭建

--- SpringCloudConfig/src/main/java/zdy/SpringApplicationSpringCloud.java
+++ SpringCloudConfig/src/main/java/zdy/SpringApplicationSpringCloud.java
@@ -3,16 +3,16 @@ package zdy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author zdy
  */
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class SpringApplicationSpringCloud {
 
     public static void main(String[] args) throws Exception {
